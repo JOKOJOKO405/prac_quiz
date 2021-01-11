7bc5655d8559a5e4a398be613dd4c65e3ee9a14e
JOKOJOKO405 <biz.kj405@gmail.com>
2021-01-11 14:49:56 +0900

    add ボタンをv-forでまわす

--- pages/quiz.vue
+++ pages/quiz.vue
@@ -3,20 +3,15 @@
     <v-col cols="12" class="mb-10">
       <v-card class="pa-7">{{ state.question }}</v-card>
     </v-col>
-    <v-col cols="12" class="mb-3">
-      <v-btn color="primary" block class="text-left">
-        {{ state.rightAnswer }}
+    <v-col
+      v-for="(answer, index) in state.allAnswers"
+      :key="index"
+      cols="12"
+      class="mb-3"
+    >
+      <v-btn :color="state.colors[index]" block class="pa-6">
+        {{ answer }}
       </v-btn>
-    </v-col>
-    <v-col cols="12" class="mb-3">
-      <v-btn color="secondary" block class="text-left">{{
-        state.wrongAnswer
-      }}</v-btn>
-    </v-col>
-    <v-col cols="12">
-      <v-btn color="accent" block class="text-left">{{
-        state.wrongAnswer2
-      }}</v-btn>
     </v-col>
   </v-row>
 </template>
@@ -29,15 +24,20 @@ import { listQuizs } from '@/src/graphql/queries'
 type QandA = {
   question: string
   rightAnswer: string
-  wrongAnswers: string[]
+  wrongAnswer: string
+  wrongAnswer2: string
+  allAnswers: string[]
+  colors: string[]
 }
 export default defineComponent({
   setup() {
-    const state = reactive({
+    const state = reactive<QandA>({
       question: '',
       rightAnswer: '',
       wrongAnswer: '',
       wrongAnswer2: '',
+      allAnswers: [],
+      colors: ['primary', 'secondary', 'accent'],
     })
     const outputQuestions = async () => {
       try {
@@ -45,11 +45,17 @@ export default defineComponent({
         const quiz: any = await API.graphql(graphqlOperation(listQuizs))
         const data = quiz.data.listQuizs.items
         console.debug(data)
-        // TODO アンサーを配列に詰め直してシャッフルする
         state.question = data[0].question
+        // TODO アンサーを配列に詰め直してシャッフルする
         state.rightAnswer = data[0].rightAnswer
         state.wrongAnswer = data[0].wrongAnswers[0]
         state.wrongAnswer2 = data[0].wrongAnswers[1]
+        state.allAnswers.push(
+          state.rightAnswer,
+          state.wrongAnswer,
+          state.wrongAnswer2
+        )
+        console.debug(state.allAnswers)
       } catch (error) {
         console.error(error)
       }
