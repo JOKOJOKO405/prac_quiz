dd3677907112efdc43f75fed9ba3fa6425c2d0fd
JOKOJOKO405 <biz.kj405@gmail.com>
2021-01-13 21:01:07 +0900

    add inject で$API使用可能に

---
+++ plugins/API.ts
@@ -0,0 +1,36 @@
+import { defineNuxtPlugin } from '@nuxtjs/composition-api'
+import { listQuizs } from '@/appsync/queries'
+
+// 各インスタンスにタイプを定義
+// TODO これなに？
+declare module 'vue/types/vue' {
+  interface Vue {
+    $API: Apis
+  }
+}
+
+declare module '@nuxt/types' {
+  interface NuxtAppOptions {
+    $API: Apis
+  }
+}
+
+// Apisの型
+interface Apis {
+  listQuizs: typeof listQuizs
+}
+
+export default defineNuxtPlugin((_, inject) => {
+  const client = async (func: (arg?: any) => Promise<any>, arg?: any) => {
+    try {
+      const data = arg ? await func(arg) : await func()
+      return data
+    } catch (e) {
+      console.error(e)
+      throw e
+    }
+  }
+  inject('API', {
+    listQuizs: () => client(listQuizs),
+  })
+})
