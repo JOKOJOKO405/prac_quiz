00e6a56109720c9216bac1ca799c546e75443393
JOKOJOKO405 <biz.kj405@gmail.com>
2021-01-13 21:13:38 +0900

    add APIプラグイン追記

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: ['~/plugins/amplify.js'],
+  plugins: ['~/plugins/amplify.js', '~/plugins/API'],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: ['@/components/', '@/templates/'],
